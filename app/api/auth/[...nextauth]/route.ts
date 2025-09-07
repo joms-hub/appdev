bc244150579619fe37ea03c50b5a1ed758835407
joms-hub <21101932@usc.edu.ph>
2025-09-07 14:20:43 +0800

    fixed auth route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 // app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@/lib/auth";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
-export const runtime = 'nodejs';
+const handler = NextAuth(authOptions);
 
-export const { GET, POST } = handlers;
+export { handler as GET, handler as POST };
